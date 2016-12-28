8a84db413c23ae357a192d787774d2af6f68a117
Maurilio Silva <mauriliodeveloper@gmail.com>
2016-12-27 23:44:43 -0200

    update

--- resources/views/painel/includes/footer.blade.php
+++ resources/views/painel/includes/footer.blade.php
@@ -7,20 +7,6 @@
     <strong>Copyright © 2016 <a href="#">M1 TRANSPORTES</a>.</strong> Todos os direitos reservados.
 </footer>
 
-<!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-        <li class="active">
-            <a href="#control-sidebar-theme-demo-options-tab" data-toggle="tab">
-                <i class="fa fa-wrench"></i>
-            </a>
-        </li>
-        <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-        <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
-
-</aside>
 <!-- /.control-sidebar -->
 <!-- Add the sidebar's background. This div must be placed
      immediately after the control sidebar -->
