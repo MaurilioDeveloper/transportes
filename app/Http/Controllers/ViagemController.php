d8dcbd12d0c25310b011c2638fac3934a7e637a4
Luiz Ceniz <luiz@betabit.com.br>
2017-01-18 17:51:56 -0200

    multiviagem

--- app/Http/Controllers/ViagemController.php
+++ app/Http/Controllers/ViagemController.php
@@ -91,7 +91,6 @@ class ViagemController extends Controller
                     INNER JOIN origens_destinos od2
                     ON f.id_cidade_destino = od2.id
                     WHERE f.status = 'Aguardando Embarque'
-                    AND NOT EXISTS (select 1 from fretes_viagens WHERE fretes_viagens.id_frete = f.id)
                        ORDER BY p.nome")
         );
 //        $fretes = Frete::query()
