1be25d4f6ad759387908e136484eed9064cb18cb
Maurilio Silva <Maurilio Silva>
2017-01-30 15:05:18 -0200

    update

--- resources/views/painel/viagens/create-edit.blade.php
+++ resources/views/painel/viagens/create-edit.blade.php
@@ -47,7 +47,13 @@
     </ol>
 @endsection
 
-<div class="overlay-loading" style="display: none;"><span>Carregando...</span></div>
+<div class="overlay-loading" style="display: none;">
+    <span style="
+        font-size: 30px;
+        font-family: cursive;
+        position: absolute;
+        margin-top: 425px;
+        margin-left: -85px">Carregando...</span></div>
 <!-- Retorna a Data do Servidor, convertendo-a em Brasileiro -->
 <div style="display: none">
     {{ \Date::setLocale('pt-BR') }}
