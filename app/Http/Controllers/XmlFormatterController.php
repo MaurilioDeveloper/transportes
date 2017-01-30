f7576165a95c0caec85dd599cdffbbe12f475685
Maurilio Silva <Maurilio Silva>
2017-01-30 18:51:40 -0200

    XmlFormatterTeste

--- app/Http/Controllers/XmlFormatterController.php
+++ app/Http/Controllers/XmlFormatterController.php
@@ -16,7 +16,19 @@ class XmlFormatterController extends Controller
 
         $csv = Writer::createFromFileObject(new \SplTempFileObject());
 //        $csv->output('viagens_'.Carbon::now().'.xml');
-        $xml = response()->xml(Viagem::query()->select()->get());
+        $viagem = Viagem::query()
+        ->join('parceiros', 'parceiros.id', '=', 'viagens.id_parceiro_viagem')
+        ->join('origens_destinos as od', 'od.id', '=', 'viagens.id_cidade_origem')
+        ->join('motoristas as m', 'm.id', '=', 'viagens.id_motorista')
+        ->join('caminhoes as c', 'c.id', '=', 'viagens.id_caminhao')
+        ->join('origens_destinos as od2', 'od2.id', '=', 'viagens.id_cidade_destino')
+        ->leftJoin('fretes_viagens as fv','fv.id_viagem','=','viagens.id')
+        ->leftJoin('fretes as f','f.id','=','fv.id_frete')
+        ->groupBy('viagens.id')
+        ->select("viagens.id", "parceiros.nome as parceiro", "m.nome as motorista", "c.modelo as caminhao", "viagens.status", "viagens.data_inicio", "od.cidade as cidade_origem", "od2.cidade as cidade_destino")
+        ->selectRaw('count(fv.id) as fretes_viagens');
+
+        $xml = response()->xml($viagem->get());
         return $xml;
 
 //        return ;
